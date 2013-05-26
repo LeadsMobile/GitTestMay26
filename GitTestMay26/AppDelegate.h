cdff324851b8ff62788dc0d40d4b9bb9216e30e6
Saleh Masum <copotron_21@yahoo.com>
2013-05-26 11:47:28 +0600

    Initial Commit

---
+++ GitTestMay26/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTestMay26
+//
+//  Created by LEADS on 5/26/13.
+//  Copyright (c) 2013 LEADS Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTestMay26/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitTestMay26
+//
+//  Created by LEADS on 5/26/13.
+//  Copyright (c) 2013 LEADS Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
