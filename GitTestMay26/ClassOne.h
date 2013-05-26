c3ff8dfdf70628e565ee13895cd738dac1c24312
Saleh Masum <copotron_21@yahoo.com>
2013-05-26 12:00:24 +0600

    A new class has been added.

---
+++ GitTestMay26/ClassOne.h
@@ -0,0 +1,13 @@
+//
+//  ClassOne.h
+//  GitTestMay26
+//
+//  Created by LEADS on 5/26/13.
+//  Copyright (c) 2013 LEADS Corporation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ClassOne : NSObject
+
+@end
---
+++ GitTestMay26/ClassOne.m
@@ -0,0 +1,13 @@
+//
+//  ClassOne.m
+//  GitTestMay26
+//
+//  Created by LEADS on 5/26/13.
+//  Copyright (c) 2013 LEADS Corporation. All rights reserved.
+//
+
+#import "ClassOne.h"
+
+@implementation ClassOne
+
+@end
